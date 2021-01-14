ceb69067314eea4e51cd0e3ae08fa05030d0a5cf
Bianca Viana <bvitoriaviana@gmail.com>
2021-01-14 16:24:18 -0300

    commit bd
    organização a partir do atendimento do professor

--- BD/PI2_criaçãoTabelas.sql
+++ BD/PI2_criaçãoTabelas.sql
@@ -1,26 +1,38 @@
 CREATE DATABASE lebi;
 USE lebi;
 
+
 CREATE TABLE Pacientes (
 	idPaciente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
 	email VARCHAR(100),
 	cpf VARCHAR(11),
-    telefone VARCHAR (10),
+    telefone VARCHAR (12),
     endereco VARCHAR (100),
     dataNascimento DATE
 );
 
 CREATE  TABLE Medicos (
-	idMedico INT AUTO_INCREMENT,
+	idMedico INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR (100),
-    idEspecialidade VARCHAR (100), 
+    especialidade VARCHAR (100),
+    CRM VARCHAR (10)
+ );   
+ 
+CREATE TABLE Agenda (
+	idAgenda INT AUTO_INCREMENT PRIMARY KEY, 
+    idMedico INT,
     
-    PRIMARY KEY (idMedico, idEspecialidade)
+	FOREIGN KEY (idMedico) REFERENCES Medicos(idMedico)
+	-- FOREIGN KEY (idPaciente) REFERENCES Pacientes(idPaciente)   
+    -- FOREIGN KEY (idAgenda) REFERENCES ConsultasMarcadas(idConsulta)
 );
 
-CREATE TABLE Especialidade (
-	idEspecialidade VARCHAR (100) 
+CREATE TABLE ConsultasMarcadas (
+	idConsulta INT AUTO_INCREMENT PRIMARY KEY,
+	idAgenda INT,
+    dataConsulta DATE,
+	horaConsulta TIME, 
 
-
+	FOREIGN KEY (idAgenda) REFERENCES Agenda(idAgenda)
 );
