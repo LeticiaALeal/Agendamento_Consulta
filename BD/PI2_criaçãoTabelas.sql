9da0d3a5428a492f3cbad129c3345926d729321d
Leticia Leal <leticia.araujoleal@gmail.com>
2021-01-19 19:36:53 -0300

    Atualização de algumas tabelas e criação de alguns inserts

--- BD/PI2_criaçãoTabelas.sql
+++ BD/PI2_criaçãoTabelas.sql
@@ -1,38 +1,36 @@
 CREATE DATABASE lebi;
 USE lebi;
 
-
 CREATE TABLE Pacientes (
-	idPaciente INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
 	email VARCHAR(100),
-	cpf VARCHAR(11),
+	cpf VARCHAR(14),
     telefone VARCHAR (12),
     endereco VARCHAR (100),
     dataNascimento DATE
 );
 
 CREATE  TABLE Medicos (
-	idMedico INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR (100),
     especialidade VARCHAR (100),
     CRM VARCHAR (10)
  );   
+
  
 CREATE TABLE Agenda (
-	idAgenda INT AUTO_INCREMENT PRIMARY KEY, 
-    idMedico INT,
-    
-	FOREIGN KEY (idMedico) REFERENCES Medicos(idMedico)
-	-- FOREIGN KEY (idPaciente) REFERENCES Pacientes(idPaciente)   
-    -- FOREIGN KEY (idAgenda) REFERENCES ConsultasMarcadas(idConsulta)
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+    horario time,
+	dia varchar(20)
 );
 
-CREATE TABLE ConsultasMarcadas (
-	idConsulta INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE agenda_medico (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	idMedico INT,
 	idAgenda INT,
-    dataConsulta DATE,
-	horaConsulta TIME, 
-
-	FOREIGN KEY (idAgenda) REFERENCES Agenda(idAgenda)
+	
+	FOREIGN KEY (idMedico) REFERENCES Medicos(id),
+	FOREIGN KEY (idAgenda) REFERENCES Agenda(id)
+	
 );
--- LeBi/LeBi/EscolhaMedico.Designer.cs
+++ LeBi/LeBi/EscolhaMedico.Designer.cs
@@ -29,19 +29,44 @@ namespace LeBi
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(116, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
             // 
             // EscolhaMedico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(616, 382);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Name = "EscolhaMedico";
             this.Text = "EscolhaMedico";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
